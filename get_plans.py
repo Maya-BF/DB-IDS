cdaf0e33f48222c5df1901dc9de2fd75a96d7987
Maya <mariem.benfadhel@gmail.com>
2017-06-08 03:48:12 -0700

    Added data transformation need to finish implementing conversion functions for each data type.

--- get_plans.py
+++ get_plans.py
@@ -3,6 +3,15 @@ import sys
 import csv
 import re
 import json
+
+list_features = []
+features = ['Node Type','Join Type','Command','Startup Cost',
+            'Total Cost','Plan Rows','Plan Width','Output','Index Cond',
+            'Order By','Recheck Cond','Filter','Workers Planned','Function Call',
+            'TID Cond','Join Filter','Merge Cond','Hash Cond','One-Time Filter',
+            'Strategy','Partial Mode','Operation','Parent Relationship',
+            'Custom Plan Provider','Parallel Aware','Schema','Alias']
+
 def get_plan():
     file_name = sys.argv[1]
     #regex to remove the first part of the query plan to leave only the
@@ -30,7 +39,7 @@ def get_plan():
                            plan = json.loads(plan)
                            plans.append(plan)
                        except ValueError:
-                           print('not a valid json structure')
+                        i   print('not a valid json structure')
     return(plans)
 
 def get_features(plan):
@@ -45,6 +54,34 @@ def get_features(plan):
             get_features(p)
     return feature_vect
 
+def valtoreal(key,value):
+    '''Assigns the right function from transtype module to the correspondent data type to transform it to real.'''
+    trans_funcs = (trans_nodetype,trans_jointype,trans_command,trans_startupcost,trans_totalcost,
+                   trans_planrow,trans_planwidth,trans_output,trans_indexcond,trans_orderby,
+                   trans_recheckcond,trans_filter,trans_workersplanned,trans_funccall,
+                   trans_tidcond,trans_joinfilter,trans_mergecond,trans_hashcond,trans_onetimefilter,
+                   trans_strategy,trans_partialmode,trans_operation,trans_parentrelationship,
+                   trans_customplanpro,trans_parallelaware,trans_schema,trans_alias)
+    #mimics a switch statement
+    trans_cases = dict(zip(features,trans_funcs))
+
+    trans_cases.get(key,)(value)
+
+def init_csv():
+    '''Set the header of a csv file with all the features'''
+    '''TO DO: Add memory related info (buckets..)'''
+
+    with open('data.csv','wb') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(features)
+    #test csv header
+    '''with open('data.csv','rb') as outcsv:
+        reader = csv.reader(outcsv)
+        for row in reader:
+            print(row)
+    '''
+#Init data csv file with header
+init_csv()
 feature_vect = []
 feature_vect.append(u'Query Text')
 for elmt in get_plan():
